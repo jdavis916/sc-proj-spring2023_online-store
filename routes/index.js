4a6ba4b2b056cf858ea190213239c74a99dca4e4
SavageShaman0 <savageshaman0@gmail.com>
2023-03-28 18:48:51 -0700

    main routes added

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page */
-router.get('/', function(req, res, next) {
+router
+.get('/', function(req, res, next) {
   res.render('index', { 
   	title: 'Simple Node Template',
   	msg: 'This sample template should help get you on your way.',
   	pageMainClass: 'pgHome'
   });
-});
+})
+//catalogue page
+.get("/catalogue", (req, res, next)=> {
+	res.render("catalogue", {
+		title: 'Catalogue',
+		msg: 'Catalogue page.',
+		pageMainClass: 'pgCatalogue'
+	})
+})
+//cart page
+.get("/cart", (req, res, next)=> {
+	res.render("cart", {
+		title: 'Cart',
+		msg: 'Cart page.',
+		pageMainClass: 'pgCart'
+	})
+})
+//contact page
+.get("/contact", (req, res, next)=> {
+	res.render("contact", {
+		title: 'Contact',
+		msg: 'Contact page.',
+		pageMainClass: 'pgContact'
+	})
+})
+//search page
+.get("/search", (req, res, next)=> {
+	res.render("search", {
+		title: 'Search',
+		msg: 'Search page.',
+		pageMainClass: 'pgSearch'
+	})
+})
+;
 
 module.exports = router;
