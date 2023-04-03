d3104a31f138460f93661644d6e376a1bcc86712
SavageShaman0 <savageshaman0@gmail.com>
2023-04-03 16:44:11 -0700

    stubs and basic html setup

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
 console.log("Script file connected");
+//logic for shopping cart
+let subtotal,
+    total,
+    sessionCart;
+//get items as a list
+
+//get item price from a list
+
+//subtotal item price and quantity
+
+//total items with tax / simple * %
